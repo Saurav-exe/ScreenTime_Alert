bcb331ab25a3423e8feb65f8a0bf43952affab0d
Saurav-exe <sauravnp0903@gmail.com>
2024-10-04 09:47:48 +0530

    Update screen_time_tracker.py
    -Bug fixed: TEXT WRAP inside the Alert BOX

--- screen_time_tracker.py
+++ screen_time_tracker.py
@@ -1,18 +1,26 @@
 import tkinter as tk
 from tkinter import Toplevel
+import ctypes 
+
+
+def hide_console():
+    # Only on Windows
+    if ctypes.windll.kernel32.GetConsoleWindow():
+        ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+
 
 def show_custom_alert():
     alert_window = Toplevel()
     alert_window.title("Take a Break!")
-    alert_window.geometry("400x200+500+300")
+    alert_window.geometry("400x200")
     alert_window.configure(bg="lightblue")
     alert_window.resizable(False, False)
     alert_window.attributes("-topmost", True)
+
     
-    # label
     label = tk.Label(
         alert_window, 
-        text="It's time to take a break!\n Have a Kit-Kat¯\_(ツ)_/¯ \nLook outside for a while to keep your eyes healthy.",
+        text="It's time to take a break!\n Have a Kit-Kat ¯\\_(ツ)_/¯ \nLook outside for a while.",
         font=("Helvetica", 16, "bold"),
         bg="lightblue",
         fg="darkblue",
@@ -20,7 +28,7 @@ def show_custom_alert():
     )
     label.pack()
 
-    # Close Button
+    # button 
     close_button = tk.Button(
         alert_window, 
         text="I'm Gonna Look Outside", 
@@ -33,13 +41,34 @@ def show_custom_alert():
     )
     close_button.pack(pady=10)
     
+    # Remove window close buttons
     alert_window.overrideredirect(True)
-    alert_window.eval('tk::PlaceWindow . center')
+
+    # Centering the window on screen
+    window_width, window_height = 400, 200
+    screen_width = alert_window.winfo_screenwidth()
+    screen_height = alert_window.winfo_screenheight()
+    
+    position_top = int(screen_height / 2 - window_height / 2)
+    position_right = int(screen_width / 2 - window_width / 2)
+    
+    alert_window.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
+
+    
+    alert_window.focus_force()
     alert_window.grab_set()
 
-root = tk.Tk()
-root.withdraw()  # Hide the root window
 
-show_custom_alert()
+def track_screen_time():
+    reminder_interval = 20 * 60 * 1000  
+    root.after(reminder_interval, show_custom_alert)  
+
+
+hide_console()
+
+
+root = tk.Tk()
+root.withdraw()  
+track_screen_time()
 
 root.mainloop()

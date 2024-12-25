57f0cfa58d43b2020ad27b49b4ca7da72f2e50b9
Saurav-exe <sauravnp0903@gmail.com>
2024-12-25 10:44:42 +0530

    Update screen_time_tracker.py
    Bug fix: Solved iteration problem

--- screen_time_tracker.py
+++ screen_time_tracker.py
@@ -2,12 +2,10 @@ import tkinter as tk
 from tkinter import Toplevel
 import ctypes 
 
-
 def hide_console():
     # Only on Windows
     if ctypes.windll.kernel32.GetConsoleWindow():
         ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
-
 
 def show_custom_alert():
     alert_window = Toplevel()
@@ -17,7 +15,6 @@ def show_custom_alert():
     alert_window.resizable(False, False)
     alert_window.attributes("-topmost", True)
 
-    
     label = tk.Label(
         alert_window, 
         text="It's time to take a break!\n Have a Kit-Kat ¯\\_(ツ)_/¯ \nLook outside for a while.",
@@ -28,7 +25,6 @@ def show_custom_alert():
     )
     label.pack()
 
-    # button 
     close_button = tk.Button(
         alert_window, 
         text="I'm Gonna Look Outside", 
@@ -40,7 +36,7 @@ def show_custom_alert():
         relief="raised"
     )
     close_button.pack(pady=10)
-    
+
     # Remove window close buttons
     alert_window.overrideredirect(True)
 
@@ -48,27 +44,24 @@ def show_custom_alert():
     window_width, window_height = 400, 200
     screen_width = alert_window.winfo_screenwidth()
     screen_height = alert_window.winfo_screenheight()
-    
+
     position_top = int(screen_height / 2 - window_height / 2)
     position_right = int(screen_width / 2 - window_width / 2)
-    
+
     alert_window.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
 
-    
     alert_window.focus_force()
     alert_window.grab_set()
 
-
 def track_screen_time():
-    reminder_interval = 20 * 60 * 1000  
-    root.after(reminder_interval, show_custom_alert)  
-
+    reminder_interval = 20 * 60 * 1000  # 20 minutes in milliseconds
+    show_custom_alert()  # Show alert now
+    root.after(reminder_interval, track_screen_time)  # Schedule the next alert
 
 hide_console()
 
-
 root = tk.Tk()
-root.withdraw()  
+root.withdraw()  # Hide the main Tkinter window
 track_screen_time()
 
 root.mainloop()
